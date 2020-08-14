00619a74c6a2d1ee051215b578a6e58dabedd00f
ysugimoto <sugimoto@wnotes.net>
2020-08-14 18:35:16 +0900

    Format source

--- jsonenv.go
+++ jsonenv.go
@@ -8,17 +8,21 @@ import (
 	"github.com/pkg/errors"
 )
 
+// jsonenv.Extract gets environment name and parse JSON, and set to environment key-value.
 func Extract(envName string) error {
 	env := os.Getenv(envName)
 	if env == "" {
 		return errors.New("Environment value is empty for: " + envName)
 	}
+
 	var kv map[string]string
 	if err := json.Unmarshal([]byte(env), &kv); err != nil {
 		return errors.Wrap(err, "Failed to unmarshal env JSON")
 	}
+
 	for key, val := range kv {
 		os.Setenv(key, val)
 	}
+
 	return nil
 }
